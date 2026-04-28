62812e0421b36dab3ffd59857160307bb55dd325
yumu-a <ayumuayumu060917@gmail.com>
2026-04-28 14:39:13 +0900

    questions.js

---
+++ questions.js
@@ -0,0 +1,10 @@
+export const quizData = {
+  easy: [
+    {q:"ボーカルは？",a:["ACAね","Ado","ikura","美波"],correct:0},
+    {q:"ZTMY特徴？",a:["匿名性","実写","男性","海外"],correct:0}
+  ],
+
+  hard: [
+    {q:"MV作家？",a:["革蝉","TV♡CHANY","Waboku","ゴル"],correct:2}
+  ]
+};
