d9715dcf87c29460304633e8c2c60adb0b46dadf
yumu-a <ayumuayumu060917@gmail.com>
2026-05-15 02:35:54 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,10 +1,69 @@
 export const quizData = {
   easy: [
     {q:"ボーカルは？",a:["ACAね","Ado","ikura","美波"],correct:0},
-    {q:"ZTMY特徴？",a:["匿名性","実写","男性","海外"],correct:0}
+    {q:"ZTMY特徴？",a:["匿名性","実写","男性","海外"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0}
   ],
-
+  normal: [
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0}
+  ],
   hard: [
-    {q:"MV作家？",a:["革蝉","TV♡CHANY","Waboku","ゴル"],correct:2}
-  ]
+    {q:"MV作家？",a:["革蝉","TV♡CHANY","Waboku","ゴル"],correct:2},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0}
+  ],
+  hell: [
+    {q:"超難問",a:["A","B","C","D"],correct:2},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0},
+    {q:"問題",a:["A","B","C","D"],correct:0}
+]
 };
